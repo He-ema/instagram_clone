0342e765df9c863c46cd035161b5c4a30afae548
ibrahim Osama <zx.hema72@gmail.com>
2024-07-03 23:44:31 +0300

    adding assets and styles

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle styleRegular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+    color: Color(0xff7D7D7D),
+  );
+  static const TextStyle styleRegular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+    color: Colors.black,
+  );
+  static const TextStyle styleRegular10 = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+    color: Color(0xff848484),
+  );
+  static const TextStyle styleRegular8 = TextStyle(
+    fontSize: 8,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+    color: Color(0xff6D6D6D),
+  );
+  static const TextStyle styleMedium17 = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Roboto',
+    color: Colors.black,
+  );
+  static const TextStyle styleMedium8 = TextStyle(
+    fontSize: 8,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Roboto',
+    color: Color(0xff23244F),
+  );
+}
---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,32 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesFacebook
+  /// assets/images/facebook.svg
+  static const String imagesFacebook = "assets/images/facebook.svg";
+
+  /// Assets for imagesHome
+  /// assets/images/home.svg
+  static const String imagesHome = "assets/images/home.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.png
+  static const String imagesLogo = "assets/images/logo.png";
+
+  /// Assets for imagesPreson
+  /// assets/images/preson.png
+  static const String imagesPreson = "assets/images/preson.png";
+
+  /// Assets for imagesProfile
+  /// assets/images/profile.svg
+  static const String imagesProfile = "assets/images/profile.svg";
+
+  /// Assets for imagesReels
+  /// assets/images/reels.svg
+  static const String imagesReels = "assets/images/reels.svg";
+
+  /// Assets for imagesSearch
+  /// assets/images/search.svg
+  static const String imagesSearch = "assets/images/search.svg";
+}
+
