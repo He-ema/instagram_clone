8221b94663f99e7e08d862a2159791fab2b537be
ibrahim Osama <zx.hema72@gmail.com>
2024-07-05 07:11:05 +0300

    form validation done

--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -7,9 +7,16 @@ import 'package:instagram_clone/features/auth/presentation/views/widgets/custom_
 import 'package:instagram_clone/features/auth/presentation/views/widgets/custom_or_widget.dart';
 import 'package:instagram_clone/features/auth/presentation/views/widgets/custom_text_form_field.dart';
 
-class SignUpViewBody extends StatelessWidget {
+class SignUpViewBody extends StatefulWidget {
   const SignUpViewBody({super.key});
 
+  @override
+  State<SignUpViewBody> createState() => _SignUpViewBodyState();
+}
+
+class _SignUpViewBodyState extends State<SignUpViewBody> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -17,68 +24,81 @@ class SignUpViewBody extends StatelessWidget {
       child: Align(
         alignment: Alignment.center,
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(Assets.imagesLogo),
-              const SizedBox(
-                height: 24,
-              ),
-              CustomTextFormField(
-                hint: 'Email',
-                controller: TextEditingController(),
-                icon: Icons.email,
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              CustomTextFormField(
-                hint: 'user name',
-                controller: TextEditingController(),
-                icon: Icons.person_outline_rounded,
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              CustomTextFormField(
-                hint: 'Bio',
-                controller: TextEditingController(),
-                icon: Icons.info_outline,
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              CustomTextFormField(
-                hint: 'Password',
-                controller: TextEditingController(),
-                isPassword: true,
-                icon: Icons.lock_outline_rounded,
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              const CustomButton(title: 'Sign up'),
-              const SizedBox(
-                height: 12,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Already have an account?',
-                    style: AppStyles.styleRegular12,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      GoRouter.of(context).pushReplacement(AppRouter.homeRoute);
-                    },
-                    child: Text(' Sign In',
-                        style: AppStyles.styleRegular12
-                            .copyWith(color: const Color(0xff23244F))),
-                  ),
-                ],
-              ),
-            ],
+          child: Form(
+            autovalidateMode: _autovalidateMode,
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(Assets.imagesLogo),
+                const SizedBox(
+                  height: 24,
+                ),
+                CustomTextFormField(
+                  hint: 'Email',
+                  controller: TextEditingController(),
+                  icon: Icons.email,
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                CustomTextFormField(
+                  hint: 'user name',
+                  controller: TextEditingController(),
+                  icon: Icons.person_outline_rounded,
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                CustomTextFormField(
+                  hint: 'Bio',
+                  controller: TextEditingController(),
+                  icon: Icons.info_outline,
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                CustomTextFormField(
+                  hint: 'Password',
+                  controller: TextEditingController(),
+                  isPassword: true,
+                  icon: Icons.lock_outline_rounded,
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                CustomButton(
+                  title: 'Sign up',
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                    } else {
+                      _autovalidateMode = AutovalidateMode.always;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Already have an account?',
+                      style: AppStyles.styleRegular12,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        GoRouter.of(context)
+                            .pushReplacement(AppRouter.homeRoute);
+                      },
+                      child: Text(' Sign In',
+                          style: AppStyles.styleRegular12
+                              .copyWith(color: const Color(0xff23244F))),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
