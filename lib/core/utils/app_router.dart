348f6a05f6886fc789e82335707c0eddf6e057ea
ibrahim Osama <zx.hema72@gmail.com>
2024-07-07 15:51:51 +0300

    adding post done

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:instagram_clone/features/add_post_and_reels/presentation/managers/cubit/add_post_cubit.dart';
 import 'package:instagram_clone/features/add_post_and_reels/presentation/views/add_post_details_view.dart';
 import 'package:instagram_clone/features/auth/presentation/views/sign_in_view.dart';
 import 'package:instagram_clone/features/auth/presentation/views/sign_up_view.dart';
@@ -28,8 +30,11 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: addPostDetailsView,
-        builder: (context, state) => AddPostDetailsView(
-          file: state.extra as File,
+        builder: (context, state) => BlocProvider(
+          create: (context) => AddPostCubit(),
+          child: AddPostDetailsView(
+            file: state.extra as File,
+          ),
         ),
       ),
     ],
--- lib/features/add_post_and_reels/presentation/views/add_post_details_view.dart
+++ lib/features/add_post_and_reels/presentation/views/add_post_details_view.dart
@@ -3,7 +3,12 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:go_router/go_router.dart';
+import 'package:instagram_clone/features/add_post_and_reels/presentation/managers/cubit/add_post_cubit.dart';
 
+import '../../../../core/utils/common_methods/awesome_dialouge.dart';
 import 'widgets/add_post_details_view_body.dart';
 
 class AddPostDetailsView extends StatefulWidget {
@@ -18,6 +23,7 @@ class AddPostDetailsView extends StatefulWidget {
 class _AddPostDetailsViewState extends State<AddPostDetailsView> {
   final TextEditingController caption = TextEditingController();
   final TextEditingController location = TextEditingController();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,15 +32,37 @@ class _AddPostDetailsViewState extends State<AddPostDetailsView> {
       appBar: AppBar(
         surfaceTintColor: Colors.white,
         backgroundColor: Colors.white,
-        title: const Text(
-          'New Post',
-          style: TextStyle(
-            color: Colors.black,
+        title: BlocListener<AddPostCubit, AddPostState>(
+          listener: (context, state) {
+            if (state is AddPostLoading) {
+              isLoading = true;
+              setState(() {});
+            } else if (state is AddPostFailure) {
+              isLoading = false;
+              setState(() {});
+              showAwesomeDialouge(context,
+                  title: 'Error', description: state.errorMessage);
+            } else {
+              isLoading = false;
+              setState(() {});
+              GoRouter.of(context).pop();
+            }
+          },
+          child: const Text(
+            'New Post',
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
         ),
         actions: [
           GestureDetector(
-            onTap: () async {},
+            onTap: () async {
+              await BlocProvider.of<AddPostCubit>(context).createPost(
+                  postImage: widget.file,
+                  caption: caption.text,
+                  location: location.text);
+            },
             child: const Text(
               'Share',
               style: TextStyle(
@@ -48,11 +76,17 @@ class _AddPostDetailsViewState extends State<AddPostDetailsView> {
           ),
         ],
       ),
-      body: AddPostDetailsViewBdoy(
-        file: widget.file,
-        caption: caption,
-        location: location,
-      ),
+      body: isLoading
+          ? const Center(
+              child: SpinKitWave(
+                color: Colors.blue,
+              ),
+            )
+          : AddPostDetailsViewBdoy(
+              file: widget.file,
+              caption: caption,
+              location: location,
+            ),
     );
   }
 }
