d67e2c78a717ad11e28722c64f8634b1317ed210
ibrahim Osama <zx.hema72@gmail.com>
2024-07-12 14:03:59 +0300

    add comment cubit done

--- lib/features/home/presentation/managers/add_comment_cubit/add_comment_cubit.dart
+++ lib/features/home/presentation/managers/add_comment_cubit/add_comment_cubit.dart
@@ -1,8 +1,51 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram_clone/constants.dart';
+import 'package:instagram_clone/features/add_post_and_reels/data/models/user_model.dart';
 import 'package:meta/meta.dart';
+import 'package:uuid/uuid.dart';
 
 part 'add_comment_state.dart';
 
 class AddCommentCubit extends Cubit<AddCommentState> {
   AddCommentCubit() : super(AddCommentInitial());
+
+  Future<void> addComment({
+    required String comment,
+    required String type,
+    required String uuid,
+  }) async {
+    emit(AddCommentLoading());
+    try {
+      var uid = const Uuid().v4();
+      UserModel user = await getUser();
+      await FirebaseFirestore.instance
+          .collection(type)
+          .doc(uuid)
+          .collection(kCommentsCollectionReference)
+          .doc(uid)
+          .set({
+        kComment: comment,
+        kUserName: user.userName,
+        kUserProfileImage: user.image,
+        kCommentId: uid,
+      });
+
+      emit(AddCommentSuccess());
+    } catch (e) {
+      emit(AddCommentFailure(errorMessage: e.toString()));
+    }
+  }
+
+  Future<UserModel> getUser() async {
+    CollectionReference users =
+        FirebaseFirestore.instance.collection(kUsersCollectionReference);
+
+    DocumentSnapshot user =
+        await users.doc(FirebaseAuth.instance.currentUser!.email).get();
+    UserModel userModel =
+        UserModel.fromJson(user.data() as Map<String, dynamic>);
+    return userModel;
+  }
 }
